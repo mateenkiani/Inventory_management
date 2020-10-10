b7835a4dd7e714008902121932f6cdd9b9c95c18
mateenkiani <kiani.mateen012@gmail.com>
2020-10-10 09:35:22 +0500

    minor code changes

--- lib/src/ViewModels/sellproductViewModel.dart
+++ lib/src/ViewModels/sellproductViewModel.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:inventory_management/src/Models/SellProduct.dart';
-import 'package:inventory_management/src/Models/product.dart';
 
 class SellProductViewModel {
   final _productsRef = FirebaseFirestore.instance.collection('sellers');
@@ -15,55 +14,5 @@ class SellProductViewModel {
     } on PlatformException catch (e) {
       print("error selling product" + e.toString());
     }
-  }
-
-  Future<void> deleteDocument(String docId) {
-    return _productsRef
-        .doc(docId)
-        .delete()
-        .then(
-          (value) => print("User Deleted"),
-        )
-        .catchError(
-          (error) => print("Failed to delete user: $error"),
-        );
-  }
-
-  Future<void> updatePrice(String docID, double price) {
-    return _productsRef
-        .doc(docID)
-        .update({
-          'price': price,
-        })
-        .then((value) => print("Price Changed"))
-        .catchError((error) => print("Failed to delete user: $error"));
-  }
-
-  Future<void> incQuantity(String docID) {
-    return _productsRef
-        .doc(docID)
-        .update({
-          'quantity': FieldValue.increment(1),
-        })
-        .then((value) => print("Count increased"))
-        .catchError((error) => print("Failed to delete user: $error"));
-  }
-
-  Future<void> decQuantity(String docID) {
-    return _productsRef
-        .doc(docID)
-        .update({
-          'quantity': FieldValue.increment(-1),
-        })
-        .then((value) => print("Count decresed"))
-        .catchError((error) => print("Failed to delete user: $error"));
-  }
-
-  Future<Product> getDoc(String docID) {
-    return _productsRef
-        .doc(docID)
-        .get()
-        .then((value) => Product.fromSnapshot(value))
-        .catchError((error) => print("Failed to delete user: $error"));
   }
 }
